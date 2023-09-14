e684dfed158e946d22724b2abd3712caf9256573
Chiko but no H <franciscotrianto@gmail.com>
2023-09-14 12:03:32 +0700

    extended praktikum finished

--- Praktikum/Praktikum_2/garbage.c
+++ Praktikum/Praktikum_2/garbage.c
@@ -1,18 +1,39 @@
+/*  Nama : Raden Francisco Trianto Bratadiningrat
+    NIM : 13522091
+    tanggal : 8 September 2023
+    Soal No : 3
+*/
+
 #include <stdio.h>
-#include "liststatik.c"
+#include "liststatik.h"
 
 int main()
 {
-    // r : list sampah
-    // p : list perjalanan
-    ListStatik s, p;
-    int i;
-    readList(&s);
-    readList(&p);
-    printList(s);
-    printf("\n");
-    printList(p);
-    printf("\n");
-
-    return 0;
+    ListStatik rumah, perjalanan;
+    readList(&rumah);
+    readList(&perjalanan);
+    insertFirst(&perjalanan, 0);
+    int sum = 0;
+    for (int i = 1; i <= 3; i++)
+    {
+        int idx = 0;
+        while (indexOf(rumah, i) != IDX_UNDEF)
+        {
+            sum += ELMT(perjalanan, idx);
+            if (ELMT(rumah, idx) == i)
+            {
+                sum += 1;
+                ELMT(rumah, idx) = 0;
+            }
+            idx++;
+            if (indexOf(rumah, i) == IDX_UNDEF)
+            {
+                for (int i = 0; i < idx; i++)
+                {
+                    sum += ELMT(perjalanan, i);
+                }
+            }
+        }
+    }
+    printf("%d\n", sum);
 }
